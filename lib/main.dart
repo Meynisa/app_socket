20ef21dc0f06d6b9b4d2b74ef70fc648e4e1ad89
Meynisa <meynisa03@gmail.com>
2020-05-08 14:51:23 +0700

    - fixed some strings

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:newflutterapp/main_detail.dart';
 import 'package:newflutterapp/socket_provider.dart';
-import 'package:newflutterapp/string.dart';
 import 'package:provider/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
@@ -12,13 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(providers: [ChangeNotifierProvider<SocketProvider>.value(value: SocketProvider())],
-    child: MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    ),);
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),);
   }
 }
 
@@ -34,8 +32,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> listMsg = [];
   IO.Socket socket;
-  int projectId = 5337;
   SocketProvider _socketProvider;
+
+  int projectId = 5337;
+  String socioToken =  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYmViYXMtc20uczQ1LmluOjgxMDAvcHcvdXNlci9sb2dpbiIsImlhdCI6MTU4ODkwNDQwMiwiZXhwIjoxNTg4OTM1NTQyLCJuYmYiOjE1ODg5MDQ0MDIsImp0aSI6Im8wTGx4a0N5Nm4rdDFya2JXWFovYVdCemJGSndURERSNVkwTDB3WkVqN1E9Iiwic3ViIjo2Mjg3LCJwcnYiOiI4N2UwYWYxZWY5ZmQxNTgxMmZkZWM5NzE1M2ExNGUwYjA0NzU0NmFhIiwiZGF0YSI6eyJ1c2VyX2lkIjo2Mjg3LCJuYW1lIjoiTGVzbGV5IEpvaG4iLCJwcm9qZWN0cyI6WzU1MTgsMCw1MzM3LDU1MjldLCJkZWZhdWx0X3Byb2plY3QiOjU1MTgsImxldmVscyI6W3sicHJvamVjdCI6NTUxOCwibGV2ZWwiOiJhZ2VudCJ9LHsicHJvamVjdCI6MCwibGV2ZWwiOiJhZ2VudCJ9LHsicHJvamVjdCI6NTMzNywibGV2ZWwiOiJhZ2VudCJ9LHsicHJvamVjdCI6NTUyOSwibGV2ZWwiOiJhZ2VudCJ9XSwiY2hhbm5lbHMiOlt7InByb2plY3QiOjU1MTgsIml0ZW1zIjpbImZhY2Vib29rIiwidHdpdHRlciIsImluc3RhZ3JhbSIsImVtYWlsIl19LHsicHJvamVjdCI6MCwiaXRlbXMiOlsiZmFjZWJvb2siLCJ0d2l0dGVyIiwiaW5zdGFncmFtIiwiZW1haWwiXX0seyJwcm9qZWN0Ijo1MzM3LCJpdGVtcyI6WyJmYWNlYm9vayIsInR3aXR0ZXIiLCJpbnN0YWdyYW0iLCJlbWFpbCJdfSx7InByb2plY3QiOjU1MjksIml0ZW1zIjpbImZhY2Vib29rIiwidHdpdHRlciIsImluc3RhZ3JhbSIsImVtYWlsIl19XSwiYWxsX3Byb2plY3RzIjpbeyJwcm9qZWN0Ijo1NTE4LCJsZXZlbCI6ImFnZW50IiwiaW5pdGlhbCI6IiJ9LHsicHJvamVjdCI6MCwibGV2ZWwiOiJhZ2VudCIsImluaXRpYWwiOiIifSx7InByb2plY3QiOjUzMzcsImxldmVsIjoiYWdlbnQiLCJpbml0aWFsIjoiIn0seyJwcm9qZWN0Ijo1NTI5LCJsZXZlbCI6ImFnZW50IiwiaW5pdGlhbCI6IiJ9XSwiaXNfYWRtaW4iOmZhbHNlLCJjbGllbnRfaWQiOjU3NjgsInRpbWV6b25lIjoiQXNpYS9KYWthcnRhIiwiaXNfcHciOnRydWV9fQ.LVScsO3AL2R5Edy2hohSKVg-4p4ea3meZXKEb45XWi0';
+  String shifId = 'Z4jmbAkrwJQK58DvK16XRde3YMlLVpGx';
+  String socket_url = 'https://push-sm.s45.in';
 
   @override
   void initState() {
@@ -45,11 +47,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _initSocket() async {
 
-    socket = IO.io("https://push-sm.s45.in", <String, dynamic>{
+    socket = IO.io("$socket_url", <String, dynamic>{
       'transports': ['websocket'],
       'query': {
-        "token": 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYmViYXMtc20uczQ1LmluOjgxMDAvcHcvdXNlci9sb2dpbiIsImlhdCI6MTU4ODkwNDQwMiwiZXhwIjoxNTg4OTM1NTQyLCJuYmYiOjE1ODg5MDQ0MDIsImp0aSI6Im8wTGx4a0N5Nm4rdDFya2JXWFovYVdCemJGSndURERSNVkwTDB3WkVqN1E9Iiwic3ViIjo2Mjg3LCJwcnYiOiI4N2UwYWYxZWY5ZmQxNTgxMmZkZWM5NzE1M2ExNGUwYjA0NzU0NmFhIiwiZGF0YSI6eyJ1c2VyX2lkIjo2Mjg3LCJuYW1lIjoiTGVzbGV5IEpvaG4iLCJwcm9qZWN0cyI6WzU1MTgsMCw1MzM3LDU1MjldLCJkZWZhdWx0X3Byb2plY3QiOjU1MTgsImxldmVscyI6W3sicHJvamVjdCI6NTUxOCwibGV2ZWwiOiJhZ2VudCJ9LHsicHJvamVjdCI6MCwibGV2ZWwiOiJhZ2VudCJ9LHsicHJvamVjdCI6NTMzNywibGV2ZWwiOiJhZ2VudCJ9LHsicHJvamVjdCI6NTUyOSwibGV2ZWwiOiJhZ2VudCJ9XSwiY2hhbm5lbHMiOlt7InByb2plY3QiOjU1MTgsIml0ZW1zIjpbImZhY2Vib29rIiwidHdpdHRlciIsImluc3RhZ3JhbSIsImVtYWlsIl19LHsicHJvamVjdCI6MCwiaXRlbXMiOlsiZmFjZWJvb2siLCJ0d2l0dGVyIiwiaW5zdGFncmFtIiwiZW1haWwiXX0seyJwcm9qZWN0Ijo1MzM3LCJpdGVtcyI6WyJmYWNlYm9vayIsInR3aXR0ZXIiLCJpbnN0YWdyYW0iLCJlbWFpbCJdfSx7InByb2plY3QiOjU1MjksIml0ZW1zIjpbImZhY2Vib29rIiwidHdpdHRlciIsImluc3RhZ3JhbSIsImVtYWlsIl19XSwiYWxsX3Byb2plY3RzIjpbeyJwcm9qZWN0Ijo1NTE4LCJsZXZlbCI6ImFnZW50IiwiaW5pdGlhbCI6IiJ9LHsicHJvamVjdCI6MCwibGV2ZWwiOiJhZ2VudCIsImluaXRpYWwiOiIifSx7InByb2plY3QiOjUzMzcsImxldmVsIjoiYWdlbnQiLCJpbml0aWFsIjoiIn0seyJwcm9qZWN0Ijo1NTI5LCJsZXZlbCI6ImFnZW50IiwiaW5pdGlhbCI6IiJ9XSwiaXNfYWRtaW4iOmZhbHNlLCJjbGllbnRfaWQiOjU3NjgsInRpbWV6b25lIjoiQXNpYS9KYWthcnRhIiwiaXNfcHciOnRydWV9fQ.LVScsO3AL2R5Edy2hohSKVg-4p4ea3meZXKEb45XWi0',
-        "shift_id": 'NqOPBjl43wKnLJ6VOL7evQ2agb8ykWXM',
+        "token": "$socioToken",
+        "shift_id": '$shifId',
       }
     });
 
@@ -62,7 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
     socket.on('connect_timeout', (data){
       print('SOCKET_TIMEOUT');
-      socket.connect();
       print('SOCKET_TRY_CONNECTING AGAIN');
     });
     socket.on('connect_error', (data){
@@ -84,7 +85,6 @@ class _MyHomePageState extends State<MyHomePage> {
         //TICKETS
         print('SOCKET_ENTRY: $data');
         listMsg.add(data["data"]["content"]);
-        dataSocket = data;
         _socketProvider.setSocketData(value: data);
         setState(() {});
       }
@@ -118,7 +118,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
-       _socketPing();
+        Navigator.push(context, MaterialPageRoute(builder: (context) => MainDetail()));
+//        _socketPing();
       }),
     );
   }
